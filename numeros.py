03cfd9fb1736c9a0d2f7a74f96052a5f06de3f04
messias1976 <messiasmachado1976@gmail.com>
2023-08-12 17:50:01 -0300

    arquivos_necessarios

--- numeros.py
+++
@@ -1,6 +0,0 @@
-print("calculator program")
-first_number = input("first number: ")
-second_number = input("second number: ")
-#print(first_number + second_number)
-
-print(int(first_number) + int(second_number))
